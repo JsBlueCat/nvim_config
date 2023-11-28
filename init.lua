c99bf22c68c536ae9cebf683fa915509275109ed
xinyi <cery.69@bytedance.com>
2023-11-28 15:02:34 +0800

    update user config

--- init.lua
+++ init.lua
@@ -22,10 +22,21 @@ return {
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
-    virtual_text = true,
-    underline = true,
+    filetypes = {"python"},
+    init_options = {
+      formatters = {
+        black = {
+          command = "black",
+          args = {"--quiet", "-"},
+          rootPatterns = {"pyproject.toml"},
+        },
+        formatFiletypes = {
+          python = {"black"}
+        }
+      }
+    }
   },
-
+  
   lsp = {
     -- customize lsp formatting options
     formatting = {
@@ -43,7 +54,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 2000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,8 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
+    local b = null_ls.builtins
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,6 +12,20 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      -- webdev stuff
+      b.formatting.deno_fmt,
+      b.formatting.prettier,
+
+      -- Lua
+      b.formatting.stylua,
+
+      -- Shell
+      b.formatting.shfmt,
+      b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+
+      -- Python
+      b.diagnostics.flake8,
+      b.diagnostics.mypy
     }
     return config -- return final config table
   end,
